e4a553a2fdd0c5c3aad11ef31b39bf5ecfb55ca8
Collin <collinsconway@gmail.com>
2019-02-03 17:21:49 -0800

    added opencv degbug flags

--- include/optic_flow/flow_controller.h
+++ include/optic_flow/flow_controller.h
@@ -3,19 +3,47 @@
 #include <ros/ros.h>
 #include <queue>
 #include <sensor_msgs/Image.h>
-
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
 namespace optic_flow
 {
     class FlowController
     {
         public:
-            FlowController();
+            FlowController(ros::NodeHandle &publicHandle, ros::NodeHandle& privateHandle);
             ~FlowController();
             void enqueueFrame(const sensor_msgs::ImageConstPtr& img);
             void process();
+
+            struct OpticFlowParams
+            {
+                int rate;
+                bool display_cv_debug;
+                OpticFlowParams() : rate{}, display_cv_debug{} {}
+                OpticFlowParams(const OpticFlowParams& params) :
+                    rate{params.rate}, display_cv_debug{params.display_cv_debug} {}
+            };
+            OpticFlowParams getParams();
+
         private:
+            ros::NodeHandle pubHandle_;
+            ros::NodeHandle priHandle_;
+            image_transport::ImageTransport transport_;
             std::queue<sensor_msgs::ImageConstPtr> img_queue_;
+
+            const OpticFlowParams params_;
+
+            image_transport::Subscriber raw_sub_;
+
             const static int MAX_SIZE = 5;
+            const static std::string RAW_WINDOW;
+
+            FlowController::OpticFlowParams loadParams();
+
+            void setupDebugWindows();
+            void displayDebugWindows();
+            void closeDebugWindows();
 
     };
 }
--- src/flow_controller.cpp
+++ src/flow_controller.cpp
@@ -2,14 +2,29 @@
 
 namespace optic_flow 
 {
-    FlowController::FlowController():img_queue_{}
+    const std::string FlowController::RAW_WINDOW = "raw image";
+
+    FlowController::FlowController(ros::NodeHandle& publicHandler,
+            ros::NodeHandle& privateHandle):
+        pubHandle_{publicHandler}, priHandle_{privateHandle},
+        params_{loadParams()},
+        transport_{pubHandle_}, img_queue_{}  
     {
         ROS_INFO("Constructing FlowController");
+        raw_sub_ = transport_.subscribe("image", 5,&FlowController::enqueueFrame, this);
+        if (params_.display_cv_debug)
+        {
+            setupDebugWindows();
+        }
     }
 
     FlowController::~FlowController()
     {
         ROS_INFO("Destructing FlowController");
+        if (params_.display_cv_debug)
+        {
+            closeDebugWindows();
+        }
     }
 
 
@@ -31,11 +46,57 @@ namespace optic_flow
                 ROS_WARN("The image queue is recieving images faster than it can process.");
             }
             auto frame = img_queue_.front();
+            if (params_.display_cv_debug)
+            {
+                auto encoding = "bgr8";
+                try
+                {
+                    cv::imshow(RAW_WINDOW, cv_bridge::toCvShare(frame, encoding)->image);
+                    cv::waitKey(30);
+                }
+                catch(cv_bridge::Exception& e)
+                {
+                    ROS_ERROR_STREAM("Could not convert from " << frame->encoding.c_str() << "to "<< encoding);
+
+                }
+            }
 	        ROS_DEBUG_STREAM("Popping Message time => " << frame->header.stamp);
             img_queue_.pop();
 
         }
         
     }
+
+
+    FlowController::OpticFlowParams FlowController::getParams()
+    {
+        return params_;
+ 
+    }
+
+    FlowController::OpticFlowParams FlowController::loadParams()
+    {
+        OpticFlowParams params{};
+        priHandle_.param("rate", params.rate, 30);
+        priHandle_.param("display_cv_debug", params.display_cv_debug, false);
+        ROS_INFO("Params:");
+        ROS_INFO_STREAM("    rate:             " << params.rate);
+        ROS_INFO_STREAM("    display_cv_debug: " << params.display_cv_debug);
+        return params;
+ 
+    }
+
+    void FlowController::setupDebugWindows()
+    {
+        cv::namedWindow(RAW_WINDOW);
+        cv::startWindowThread();
+    }
+
+    void FlowController::closeDebugWindows()
+    {
+        cv::destroyWindow(RAW_WINDOW);
+        
+    }
+
 
 }
